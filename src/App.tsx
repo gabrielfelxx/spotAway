ba953d4960c8419316adb892ca2317476120a710
gabrielfelxx <gabrielfelxx@gmail.com>
2023-11-09 21:27:56 -0300

    feat: add hero and cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import './styles/App.scss'
-import Header from './components/Header/index'
-import ImgHero from './components/ImgHero/index'
+import "./styles/App.scss";
+import Main from "./pages/main";
 
 function App() {
-
   return (
     <>
-      <Header/>
-      <ImgHero/>
+      <Main />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ImgHero/index.tsx
+++
@@ -1,9 +0,0 @@
-import './style.scss'
-
-export default function ImgHero() {
-  return (
-    <>
-    <img src="./test.png" alt="test" />
-    </>
-  )
-}
