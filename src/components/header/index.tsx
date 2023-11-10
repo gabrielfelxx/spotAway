1c39c3ed940a2c1a7a639d9714ecb8d9213ff0a8
Nicolas Braga <eunicolasbraga@gmail.com>
2023-11-10 10:47:21 -0300

    feat: adjustment in responsiveness and creation of the sideBar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,31 +1,58 @@
+import React, { useState } from "react";
+import LoginDropdown from "../LoginDropdown";
+import Sidebar from "./components/SideBar";
+import { AlignJustify } from "lucide-react";
 import "./style.scss";
 
-function Header() {
-  const reloadPage = () => {
-    window.location.reload();
+const Header: React.FC = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [sidebarIsActive, setSidebarIsActive] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const showSidebar = () => {
+    setSidebarIsActive(!sidebarIsActive);
+  };
   return (
-    <>
-      <nav className="header" onClick={reloadPage}>
-        <div className="headerList">
-          <ul className="list">
-            <li>Home</li>
-            <li>Emphasis</li>
-            <li>Category</li>
-          </ul>
-        </div>
-
+    <div>
+      <nav className="header">
         <div className="logo">
-          <h2 className="logoText">SpotAway</h2>
+          <h2>SpotAway</h2>
         </div>
 
-        <div className="login">
-          <button className="loginButton">Sign in</button>
+        <div className="containerItemsHeader">
+          <div className="headerList">
+            <ul>
+              <li>Home</li>
+              <li>Emphasis</li>
+              <li>Category</li>
+            </ul>
+          </div>
+
+          <div className="buttonLoginContainer">
+            <button onClick={toggleDropdown}>Sign in</button>
+            {isDropdownOpen && <LoginDropdown onClose={toggleDropdown} />}
+          </div>
+
+          {/* Botão de hambúrguer independente do Sidebar */}
+          <div className="sidebarActive">
+            <button onClick={showSidebar}>
+              <AlignJustify size={30} />
+            </button>
+          </div>
         </div>
+
+        {/* Renderiza o Sidebar se estiver ativo */}
+        {sidebarIsActive && (
+          <Sidebar
+            setSidebarIsActive={setSidebarIsActive}
+            sidebarIsActive={sidebarIsActive}
+          />
+        )}
       </nav>
-    </>
+    </div>
   );
 }
-
 export default Header;
