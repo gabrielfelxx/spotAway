fa0c4b44777125f4056e9baf00dfe20791fa7608
gabrielfelxx <gabrielfelxx@gmail.com>
2023-11-10 22:15:07 -0300

    feat: add cards in places page

--- src/pages/Places/index.tsx
+++ src/pages/Places/index.tsx
@@ -1,9 +1,15 @@
 import './style.scss'
+import Card from '../HomePage/components/cards'
+import { cards } from '../HomePage/components/emphasis/'
 
 export default function Places() {
   return (
     <div>
-        <h2>test</h2>
+        <div className="cards">
+        {cards.map((card) => (
+          <Card key={card.id} {...card} />
+        ))}
+        </div>
     </div>
   )
 }
