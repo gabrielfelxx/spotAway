71d56bb10f3fd97943aa37be202ee0d4adda4fcf
gabrielfelxx <gabrielfelxx@gmail.com>
2023-11-10 21:17:50 -0300

    feat: add error page

---
+++ src/pages/ErrorPage/index.tsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+
+export default function ErrorPage() {
+  return (
+    <div className="errorDiv">
+      <h2>ERROR 404!</h2>
+      <span>sai daqui kkk</span>
+    </div>
+  );
+}
