4e380840dd04e02f11cc49941d0bae8a5edf6082
gabrielfelxx <gabrielfelxx@gmail.com>
2023-11-10 22:42:47 -0300

    feat: add responsive cards to place page

--- src/pages/Places/index.tsx
+++ src/pages/Places/index.tsx
@@ -1,9 +1,19 @@
-import './style.scss'
+import "./style.scss";
+import Header from "../../components/header";
+import Card from "../HomePage/components/cards";
+import { cards } from "../HomePage/components/emphasis/";
 
 export default function Places() {
   return (
-    <div>
-        <h2>test</h2>
+    <div className="placesContainer">
+      <Header />
+      <div className="placeCardsDiv">
+        <div className="placeCards">
+          {cards.map((card) => (
+            <Card key={card.id} {...card} />
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
