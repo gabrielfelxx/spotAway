685b04772190af3b5cbcc93f85a82b6f1542a315
gabrielfelxx <gabrielfelxx@gmail.com>
2023-11-10 21:16:58 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import "./styles/App.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Home from "./pages/HomePage";
+import Places from "./pages/Places";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: "places",
+    element: <Places />,
+    errorElement: <ErrorPage/>,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Home />
+      <RouterProvider router={router} />
     </>
   );
 }
